5feb04e4b1bbeb4a891a245fbddb3f19b20cf62c
Ludovic Lafrogne <ludoyoreo@hotmail.com>
2025-10-05 23:44:13 +0200

    landing page draft

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Main = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='p-5 text-center bg-light-dark rounded'>
+                <h1 className='text-light'>Stock Predicion App</h1>
+                <p className='text-light lead'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nam purus sapien, pretium pulvinar lacus ac, molestie mollis justo. 
+                    Donec condimentum vel est vel faucibus. Praesent commodo cursus eleifend. 
+                    Etiam pellentesque erat eros, ac suscipit nibh venenatis ut. 
+                    Nullam convallis rutrum facilisis. Mauris lobortis justo at mauris volutpat, non pellentesque dui malesuada. 
+                    Quisque diam est, rutrum in mattis ac, pretium non elit. Aenean a rutrum neque. 
+                    Pellentesque nisi arcu, scelerisque vel enim vitae, accumsan pretium ipsum. 
+                    Integer facilisis, justo quis tincidunt elementum, augue mi laoreet erat, ut rutrum quam nulla in neque. 
+                    Vivamus sem ipsum, elementum vitae faucibus tempor, gravida eu lacus.</p>
+                <a className='btn btn-info' href="">Login</a>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Main
